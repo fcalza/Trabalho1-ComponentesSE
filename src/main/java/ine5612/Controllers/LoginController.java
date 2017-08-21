d4196a9c091af3ce30e2b03fa915f0c188dcec06
vinicamel <vinicamel>
2017-08-21 19:52:15 -0300

    First commit

---
+++ src/main/java/ine5612/Controllers/LoginController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5612.Controllers;
+
+/**
+ *
+ * @author 16200923
+ */
+public class LoginController {
+    
+}
